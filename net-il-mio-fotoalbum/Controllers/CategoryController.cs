9dab0fe34987366c016c5c30dabf4fafbea90841
Brody14 <stefania.spinas@gmail.com>
2023-10-12 12:39:04 +0200

    FEAT category index view and controller + edit index photo

---
+++ net-il-mio-fotoalbum/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using net_il_mio_fotoalbum.Database;
+using net_il_mio_fotoalbum.Models;
+using System.Linq;
+
+namespace net_il_mio_fotoalbum.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private PhotoContext _myDatabase;
+
+        public CategoryController(PhotoContext db)
+        {
+            _myDatabase = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _myDatabase.Categories.ToList<Category>();
+
+            return View("Index", categories);
+        }
+    }
+}
